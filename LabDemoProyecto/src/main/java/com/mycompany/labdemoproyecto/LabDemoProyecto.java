7105f54b1c935eac70b8827e6c381c994f3704de
bcerdas <bryan.cerdas07@gmail.com>
2026-05-19 20:51:21 -0600

    base del proyecto

---
+++ LabDemoProyecto/src/main/java/com/mycompany/labdemoproyecto/LabDemoProyecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.labdemoproyecto;
+
+/**
+ *
+ * @author bryan
+ */
+public class LabDemoProyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
